a8b2163799400c98980d1c3928c5058e78d2691b
Venkatesh <venkeyp6@gmail.com>
2024-06-18 15:09:10 +0530

    tokenization redone to suit the dataset

--- analysis.py
+++ analysis.py
@@ -3,16 +3,18 @@ import glob
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import seaborn as sns
-from nltk.tokenize import word_tokenize, MWETokenizer
+from nltk.tokenize import word_tokenize
+from nltk.tokenize.mwe import MWETokenizer
 from nltk.corpus import stopwords
 from IPython.display import set_matplotlib_formats
+import string
 
 
 # Uncomment on first run
-# import nltk
+# 
+import nltk
 # nltk.download('punkt')
 # nltk.download('stopwords')
-
 def split_job_type(job_type_str):
     parts = job_type_str.split(' ')
     if len(parts) == 4:
@@ -42,21 +44,31 @@ def filtered_df(jobs_all, job_title, job_level):
     # create new column for tokenized words
     jobs_filtered['tokenized_details'] = ""
     for index, row in jobs_filtered.iterrows():
-        # lowercase words
-        detail = row.job_details
-        # tokenize words
-        detail = word_tokenize(detail)
-        # handle multi-word tokenization (e.g., Power BI)
-        tokenizer = MWETokenizer([('power', 'bi'), ('scala', 'sucks')])
-        detail = tokenizer.tokenize(detail)
-        # remove duplicates
-        detail = list(set(detail))
-        # remove stopwords & numbers/punction
-        detail = [word for word in detail if word not in stopwords.words('english')] 
-        # add to details list
-        row.tokenized_details = detail
-    
+        detail = row.job_details.lower() 
+        values = detail.split(',')
+        if len(values) > 1:
+            last_value = values[-1].strip()
+            if last_value.startswith("and "):
+                values[-1] = last_value[4:]
+        jobs_filtered.loc[index, 'tokenized_details'] = ' '.join(values)
     return jobs_filtered 
+
+# def word_cloud(jobs_filtered, keywords):
+#     # convert df column to list
+#     analyst_list = jobs_filtered.tokenized_details.tolist()
+#     print(analyst_list)
+#     # flatten list of lists
+#     analyst_list = [item for sublist in analyst_list for item in sublist]
+#     # filter list
+#     analyst_list = [item for item in analyst_list if item in keywords]
+#     # convert for wordcloud
+#     analyst_list = " ".join(words for words in analyst_list)
+#     # wordcloud the results
+#     wc_join = WordCloud(background_color="white", collocations=False).generate(analyst_list)
+#     plt.imshow(wc_join, interpolation='bilinear')
+#     plt.axis("off")
+#     plt.show()
+#     return
 
 path = 'output'
 files = glob.glob(path + "/*.csv")
@@ -94,8 +106,17 @@ jobs_all[['job_time', 'job_level', 'job_type']] = jobs_all['job_type'].apply(spl
 # print(jobs_all.job_level.value_counts().head(4))
 
 #ANALYSIS
+#Datafame filtering
 analyst_entry = filtered_df(jobs_all, "Analyst", "Entry-level")
 analyst_entry.to_csv('output_fileaa.csv', header=True, index=True)
 
+# #Wordcloud of keywords
+# keywords_analyst = ['excel', 'sql', 'microsoft', 'tableau',  'python', 'word', 
+# 'powerpoint', 'r',  'slack', 'coding', 'looker',
+# 'outlook', 'azure', 'jira', 'twilio', 'server', 'snowflake', 'ai', 'warehousing', 'scrum',
+# 'powerbi', 'shell', 'linux', 'sas', 'sharepoint', 'devops', 'mysql', 'c', 'visio', 
+# 'javascript', 'git', 'mssql', 'vba', 'powerpoints', 'java', 'postgresql', 'spreadsheets',
+# 'pandas', 'gdpr', 'elt', 'scala', 'css', 'spreadsheet', 'alteryx', 'github', 'postgres', 'power_bi', 'spss']
+# word_cloud(analyst_entry, keywords_analyst)
 
 
